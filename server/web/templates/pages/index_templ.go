5a523c0f5db45a5438ff03d3627bdca8333e81e7
Uhrbaan <uhrbaan@gmx.net>
2025-06-20 23:13:50 +0200

    Removed broken graph.

--- server/web/templates/pages/index_templ.go
+++ server/web/templates/pages/index_templ.go
@@ -60,14 +60,6 @@ func Index(name string) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, " <h1>Historique</h1>")
-			if templ_7745c5c3_Err != nil {
-				return templ_7745c5c3_Err
-			}
-			templ_7745c5c3_Err = components.Graph().Render(ctx, templ_7745c5c3_Buffer)
-			if templ_7745c5c3_Err != nil {
-				return templ_7745c5c3_Err
-			}
 			return nil
 		})
 		templ_7745c5c3_Err = layouts.Base("Index").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
